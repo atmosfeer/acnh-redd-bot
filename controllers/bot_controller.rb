39ecf92ae5bb9aff4ff8dffd03a5cd8d1d1f7695
Nicolas Feer <atmosfeer@gmail.com>
2020-06-14 17:05:36 +0200

    fixed message duplication on reactions

--- controllers/bot_controller.rb
+++ controllers/bot_controller.rb
@@ -3,11 +3,14 @@ require 'pry-byebug'
 class BotController
   def initialize(bot)
     @bot = bot
+    add_reaction_event_listener
+    remove_reaction_event_listener
   end
 
   def redd_command(event)
       user = set_user(event)
       # channel = Channel.find_by_discord_id(event.channel.id)
+
       return "Sorry you already have an active post, delete that first with d!delete before creating a new one." if user.active_post
       return "Hmm. Please check if your post matches the template in #art-announcements or type d!template" unless event.content.include?("1.")
       return "Sorry you're in the queue for a different post, please finish that before you enter a queue!" if user.in_queue
@@ -22,20 +25,26 @@ class BotController
       bot_message = @bot.send_message(ENV['CHANNEL_ID'], "", nil, { description: announcement_message.original_message_no_art, fields: announcement_message.build_inline_fields, color: 0x12457E } )
       announcement_message.discord_id = bot_message.id
       announcement_message.save!
-
-      add_reaction_event_listener(bot_message)
-      remove_reaction_event_listener(bot_message)
       add_edit_event_listener(bot_message)
 
-      @bot.send_message(event.channel.id, "", nil, { description: "Succesfully created! Check out #art-announcements. Use d!queue <dodo_code> to activate the post!", color: 0x12457E } )
 
+      @bot.send_message(event.channel.id, "", nil, { description: "Succesfully created! Check out #art-announcements. Use d!queue <dodo_code> to activate the post!", color: 0x12457E } )
+      #### delete this before deployment  ####
+      queue_command(event)
+      ########################################
       nil
   end
 
   def queue_command(event)
       channel = @bot.channel(ENV['CHANNEL_ID'])
-      dodo = event.content.downcase.gsub("d!queue","").strip.match(/\w{5}/).to_s.upcase
-      event.message.delete
+      ### Uncomment before deployment
+      # dodo = event.content.downcase.gsub("d!queue","").strip.match(/\w{5}/).to_s.upcase
+      # event.message.delete
+      # ###############################
+
+      ### Delete before deployment ######
+      dodo = ("A".."Z").to_a.shuffle[0..4].join
+      ###################################
       if dodo.empty?
         @bot.send_message(event.channel.id, "", nil, { description: "Oops, I think your forgot the dodo code, or maybe you typed it wrong? Try again! Remember it's d!queue <dodo_code>. No brackets." , color: 0x12457E } )
         return nil
@@ -133,7 +142,7 @@ class BotController
     user ? user : User.create!(discord_id: event.user.id, discord_name: event.user.username)
   end
 
-  def remove_reaction_event_listener(bot_message)
+  def remove_reaction_event_listener
     EMOJIS.each_with_index do |emoji, i|
       @bot.reaction_remove(attributes = { emoji: emoji }) do |event|
         user = set_user(event)
@@ -144,34 +153,36 @@ class BotController
           reaction.destroy! unless art_piece.status == "bought"
         end
         art_piece.update_status unless art_piece.status == "bought"
-        bot_message.edit("", { description: reacted_message.original_message_no_art, fields: reacted_message.build_inline_fields })
+        event.message.edit("", { description: reacted_message.original_message_no_art, fields: reacted_message.build_inline_fields })
         user.in_queue = false
         user.save!
       end
     end
   end
 
-  def add_reaction_event_listener(bot_message)
+  def add_reaction_event_listener
     EMOJIS.each_with_index do |emoji,i|
-      @bot.reaction_add(attributes = { emoji: emoji }) do |event|
+      @bot.reaction_add(emoji: emoji) do |event|
+        # log_path = "/Users/atmosfeer/code/acnh-redd-bot/reaction_log.txt"
+        # f = File.open(log_path, 'a')
+        # f.write("user: #{user.discord_name} | event.message.id #{event.message.id} | bot_message_id #{bot_message.id} \n\n")
         user = set_user(event)
         reacted_message = Announcement.find_by_discord_id(event.message.id)
-        binding.pry
         if user.in_queue
           event.user.pm("Ooops! You're already in a another queue.")
-          bot_message.delete_reaction(user.discord_id, emoji)
+          event.message.delete_reaction(user.discord_id, emoji)
           return nil
         end
         art_piece = reacted_message.art_pieces.where(number: i + 1).first
         if user.can_react?(art_piece)
           Reaction.create!(number: i + 1, announcement: reacted_message, user: user)
           art_piece.update_status
-          bot_message.edit("", { description: reacted_message.original_message_no_art, fields: reacted_message.build_inline_fields })
+          event.message.edit("", { description: reacted_message.original_message_no_art, fields: reacted_message.build_inline_fields })
           event.user.pm("Get ready pick up your art at #{reacted_message.user.mention}'s island! Dodo code is: #{reacted_message.dodo}")
           user.in_queue = true
           user.save!
         else
-          bot_message.delete_reaction(user.discord_id, emoji)
+          event.message.delete_reaction(user.discord_id, emoji)
           event.user.pm("Sorry! You can't claim this! If you don't know why, please ask a moderator!")
         end
       end
---
+++ log_test.rb
@@ -0,0 +1,3 @@
+log_path = "/Users/atmosfeer/code/acnh-redd-bot/reaction_log.txt"
+f = File.open(log_path, 'a')
+f.write("yearha")
